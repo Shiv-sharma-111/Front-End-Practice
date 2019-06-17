bd910f9f2156e63de760e181d06df3c8448d6e27
Shiv Sharma <shivsharmavictory@gmail.com>
2019-06-17 14:11:43 +0530

    javascript

---
+++ js/j1.js
@@ -0,0 +1,10 @@
+document.write("Ax Shivam sharma");
+function fun()
+{
+  var i;
+  for(i=0;i<50;i++)
+  {
+    document.write("i="+i+"<br/>");
+  }
+}
+//var keyword is used to defined the variable
