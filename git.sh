fa8f85287b1c70d5f0038a5adca7f41dad7d5a61
Shiv Sharma <shivsharmavictory@gmail.com>
2019-06-25 14:01:57 +0530

    Update

---
+++ git.sh
@@ -0,0 +1,6 @@
+#Use like a pro user
+#change the commit message according to your condition
+clear
+git add --all
+git commit -m "Update"
+git push
--- js/j1.js
+++ js/j1.js
@@ -1,10 +1,30 @@
 document.write("Ax Shivam sharma");
 function fun()
 {
-  var i;
-  for(i=0;i<50;i++)
+  var x =document.getElementById('text1');
+  var y =document.getElementById('text2');
+  var z=document.getElementById('heading3');
+  if(x.checked==true)
   {
-    document.write("i="+i+"<br/>");
+    z.style.color="red";
+    alert("The Channel "+x.value);
   }
+  else if (y.checked==true) {
+    alert("The Channel "+y.value)
+  }
+  else {
+    alert("No Channel is selected");
+  }
+  /*var x =document.getElementById('text1').value;
+  var y =document.getElementById('text2').value;
+  if(x==y)
+  {
+    alert("mathes");
+  }
+  else {
+          alert("Not mathes");
+      }*/
+  //alert(x);
+   //document.getElementById('heading3').innerHTML="hii this is shiavm sharma";
 }
 //var keyword is used to defined the variable
